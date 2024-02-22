15096ab267daa901e746be280b95eb812ca04cf3
venkata2105/ <venkatanaveen.prabha5454@gmail.com>
2024-02-22 16:16:41 +0530

    blogprojectadded

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-#from django.contrib.auth.models import User
+
+
+# from django.contrib.auth.models import User
 
 
 class User(models.Model):
@@ -9,6 +11,7 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
 
 '''Post: Fields for title, content, author, category, created_date.
 ● Comment: Fields for post, author, content, created_date.
@@ -34,4 +37,3 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.AUTHOR} commented {self.CONTENT}"
-
